20f319dfaa1fe6393ab549ab0a5ac373e33992d8
timothee-dlh <108541810+timothee-dlh@users.noreply.github.com>
2023-09-08 08:59:19 +0200

    Add files via upload

--- trieur.c
+++ trieur.c
@@ -1,8 +1,14 @@
+/*
+To Do :
+Remove all printf statements for the final version ( not needed)
+Error : main.c:81:17: error: label ‘nextFile’ used but not defined
+*/
 #include "stdio.h"
 #include "dirent.h"
 #include "errno.h"
 #include "string.h"
 
+void removePointDir(char* filename);
 int main(int argc, char* argv[]){
 
       struct iterLoop {
@@ -15,57 +21,65 @@ int main(int argc, char* argv[]){
       iter.MAX_CHAR = 300; iter.MAX_FILES = 1000;
 
       struct dirent *pDirent;
-
-      char* filename;
-      char extension [10];
-      DIR *pDir;      
+	  DIR *pDir; 
       
+	  char* filename;
+      char extension [10]; // extension of file
+       
       if (argc != 2){
             printf("Please specify the directory name in argument");
             return 1;
       }
 
-      pDir = opendir (argv[1]);
+      pDir = opendir (argv[1]); // open directory
       if (pDir == NULL){
             printf("Cannot open the directory\n");
             return 1;
       }
       
-      while ((pDirent = readdir(pDir)) != NULL && iter.iterFiles < iter.MAX_FILES ){
+      while ((pDirent = readdir(pDir)) != NULL && pIter->iterFiles < pIter->MAX_FILES ){
             filename = pDirent->d_name;
             
             while (*filename != '\0' && iter.iterChar < iter.MAX_CHAR){
-                  if(strcmp(filename, ".") != 0 && strcmp(filename, "..") != 0){
-                        (void)printf("%c", *filename);
+                        removePointDir(filename);
+						(void)printf("%c", *filename);
                        
                         if(*filename == '.'){
                        
                               (void)printf("point detected\n");
                               filename ++; 
-                       
-                              for (int i; *filename != '\0' || *filename == ' '; i ++){
+							
+                              for (int i = 0; *filename != '\0' || *filename == ' '; i ++){
                                    extension[i] = *filename;
                                    (void)printf("%c", extension[i]);
                                    filename ++;
                                }              
                         } 
-                  }else{
-                       goto nextFile;
-                  } 
-                  // Extension stockée dans extension
-                  iter.iterChar ++; filename ++;
-                  for(int i; i <= strlen(extension); i++){
-                      strcpy(extension, "");
+                 
+                  // Extension stored in  char extension [10];
+                  pIter->iterChar ++; filename ++;
+				
+				  for(int i; i <= strlen(extension); i++){ // Reset of char extension [10];
+				  strcpy(extension, "");
                   }
             }
                   
             (void)printf("\n"); 
-nextFile:     
-             pIter->iterFiles ++, pIter->iterChar = 0; 
+nextFile:     // goto if not a file
+            
+			pIter->iterFiles ++, pIter->iterChar = 0; // next file
       }
       if(closedir(pDir) != 0){
             printf("Can't close the directory");
             return 1;
       }
-      return 0;
+      
+	  return 0;
+}
+
+void removePointDir(char *filename){
+	if(strcmp(filename, ".") == 0 && strcmp(filename, "..") == 0){
+		goto nextFile;
+	}
+
 }
